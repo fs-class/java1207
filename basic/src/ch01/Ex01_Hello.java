3bcf3b0f5872da3555e718e566c073989558270a
fs-class <whyeil29@gmail.com>
2025-12-07 08:11:19 +0900

    자바기초

---
+++ basic/src/ch01/Ex01_Hello.java
@@ -0,0 +1,38 @@
+package ch01;
+
+/**
+ * 도큐먼트 주석
+ * @author 작성자명
+ * 기능: 자바 시작하기
+ * 작성일: 2025.12.07
+ * @param args
+ */
+/*
+ * Hello는 클래스명
+ * 클래스명은 클래스 파일명과 같아야 한다.
+ */
+public class Ex01_Hello {
+	/*
+	 * 단축키
+	 * ctrl+shift+L: 단축키 목록
+	 * ctrl+M: 편집창 최대화/복원
+	 * alt+↑↓: 선택한 라인 위/아래 이동
+	 * ctrl+alt+↑↓: 선택한 라인 복제
+	 * ctrl+d: 선택한 영역 또는 줄 삭제
+	 * ctrl+space: 코드 자동 완성
+	 * ctrl+/: 선택한 영역 한 줄 주석
+	 * ctrl+shift+/: 선택한 영역 블록 주석 -> /*엔터
+	 * alt+Shift+J: 문서 주석 -> /**엔터
+	 * ctrl++/-: 편집창 글꼴 확대/축소
+	 * ctrl+i 또는 ctrl+shift+f: 자동 들여쓰기/정렬
+	 * ctrl+F11: 실행
+	 * alt+shift+y: 줄 바꿈
+	 * F2 또는 alt+shift+R: 이름 바꾸기
+	 * 범위 주석 main 메서드: 프로그램 실행 진입점
+	 */
+	public static void main(String[] args) {
+		// 콘솔 출력
+		System.out.println("Hello, Java");
+	}
+
+}
---
+++ basic/src/ch01/Ex02_실행문.java
@@ -0,0 +1,21 @@
+package ch01;
+
+/*
+ * 실행문? 
+ * - 변수 선언
+ * - 변수값 저장
+ * - 메서드 호출
+ * - 내용 출력
+ * 실행문 끝에는 세미콜론(;)을 붙인다.
+ */
+public class Ex02_실행문 {
+	public static void main(String[] args) {
+		// ctrl+space: 자동 완성
+		int x;
+		x = 1;
+		int y = 2;
+		int result = x + y;
+		System.out.println(result);
+		// ctrl+F11: 실행
+	}
+}
